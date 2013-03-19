f15471c03cb5cccf54b1aa793d47c32d149aba8c
Jack mao <b39360@freescale.com>
2013-03-19 13:46:37 +0800

    ENGR00232913 Wifi: enable realtek wifi
    update wifi realtek specific files.
    
    Signed-off-by: Jack mao <b39360@freescale.com>

--- wlan/config/Android.mk
+++ wlan/config/Android.mk
@@ -27,15 +27,3 @@ include $(BUILD_PREBUILT)
 
 ########################
 
-WIFI_DRIVER_SOCKET_IFACE := wlan0
-ifeq ($(strip $(WPA_SUPPLICANT_VERSION)),VER_0_8_X_RTL)
-  include external/wpa_supplicant_8_rtl/wpa_supplicant/wpa_supplicant_conf.mk
-else
-ifeq ($(strip $(WPA_SUPPLICANT_VERSION)),VER_0_6_X)
-  include external/wpa_supplicant_6/wpa_supplicant/wpa_supplicant_conf.mk
-else
-  include external/wpa_supplicant/wpa_supplicant_conf.mk
-endif
-endif
-
-########################
